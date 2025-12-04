e219d1f5c4dc972287a9d2eb8df3fdf3736569d8
akashmadha <akshmadha321@gmail.com>
2025-12-04 19:43:25 +0530

    Added home route and updated URLs for Render deployment

--- Backend/urls.py
+++ Backend/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Pinterest Clone Backend Running Successfully</h1>")
+
 
 urlpatterns = [
+     path('', home),     
     path('admin/', admin.site.urls),
     path('api/', include('pinterest_App.urls')),
      path('accounts/', include('allauth.urls')),
